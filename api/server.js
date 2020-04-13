9f7f8696fb4e458d54969012d36feaa6138c3c46
Naomi Ruemmely Torres <59476335+NaomiPriest97@users.noreply.github.com>
2020-04-13 16:50:44 -0400

    Saving my queries

--- api/server.js
+++ api/server.js
@@ -6,4 +6,37 @@ const server = express();
 
 server.use(express.json());
 
+server.get('/api/accounts', (req, res) => {
+    db.select('*')
+    .from('accounts')
+    .then(accounts => {
+        res.status(200).json({
+            data: accounts
+        });
+    })
+    .catch(error => {
+        console.log(error);
+        res.status(500).json({
+            message: error.message
+        });
+    });
+});
+
+server.get('/api/accounts/:id', (req, res) => {
+    db('accounts')
+    .where({ id: req.params.id })
+    .first()
+    .then(account => {
+        res.status(200).json({
+            data: account
+        });
+    })
+    .catch(error => {
+        console.log(error);
+        res.status(500).json({
+            message: error.message
+        });
+    });
+});
+
 module.exports = server;
